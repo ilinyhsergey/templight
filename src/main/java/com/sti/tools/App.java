98cc9b18cc4d693c3d2d9446e5f0deabbd65f1b0
Sergey Ilinykh <sergeyi@speaktoit.com>
2014-10-02 11:59:10 +0700

    project structure

---
+++ src/main/java/com/sti/tools/App.java
@@ -0,0 +1,14 @@
+package com.sti.tools;
+
+/**
+ * Hello world!
+ */
+public class App {
+    /**
+     *
+     * @param args template_filename output_filename var1=val1 var2=val2 ...
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/sti/tools/AppTest.java
@@ -0,0 +1,40 @@
+package com.sti.tools;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.net.URL;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+        extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+        URL resource = AppTest.class.getResource("/template.html");
+        assertNotNull(resource);
+        System.out.println("+++++++++++++++++++++++++++++++++++"+resource.toString());
+
+    }
+}
