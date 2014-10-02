d92e6c21703afbe7b2975b48124a2b015fec45ee
Sergey Ilinykh <sergeyi@speaktoit.com>
2014-10-02 18:37:21 +0700

    path

---
+++ src/main/java/com/sti/tools/templight/Templater.java
@@ -0,0 +1,14 @@
+package com.sti.tools.templight;
+
+import java.io.File;
+import java.util.Map;
+
+public class Templater {
+    public static boolean execute(File src, File dst, Map args) {
+
+
+
+
+        return true;
+    }
+}
